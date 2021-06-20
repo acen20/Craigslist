b5dea135dd7f47d5bbfd58a47554bb44d54a49ea
acen20 <ahsen1330@gmail.com>
2021-06-20 13:11:55 +0500

    Project set up

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'myapp/index.html'
+    context_object_name = 'a'
+
+    def get_queryset(self):
+        return "The page is finally Live"
